b77b298d08fa8a1f6f6453944f064b4927c22302
David Morris <gm3dmo@github.com>
2023-09-22 15:57:55 +0100

    Adding army number view for #4

---
+++ cmp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+#from .views import index
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"), 
+    path("sentry-debug/", views.trigger_error ),
+    path("tools/army-number-search", views.original_unit, name="army-number-search" ),
+    path("tools/army-number-search/<int:army_number>"  , views.original_unit, name="army-number-search" ),
+]
--- core/urls.py
+++ core/urls.py
@@ -18,12 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
-    path('sentry-debug/', trigger_error),
 ]
+urlpatterns += [path("", include("cmp.urls"))]
