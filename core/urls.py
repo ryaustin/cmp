78132cfecf9d7953ba3a0956b687029754ed66f9
David Morris <gm3dmo@github.com>
2023-09-30 13:01:54 +0100

    Adding template and Fell fonts

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
+
 urlpatterns += [path("", include("cmp.urls"))]
