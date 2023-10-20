c261df7153b0ef3959ae84c6996d4951281a9aee
David Morris <gm3dmo@github.com>
2023-10-20 22:08:32 +0100

    Adding ipdb back in the mix.

--- cmp/country-import-to-model.py
+++ cmp/country-import-to-model.py
@@ -1,18 +1,11 @@
 
-# read csv.py
-# read csv_file and create a country model for each row in the csv file
-# the csv_file has the format "id,name,alpha2,alpha3,country_code"
+# read countries.json
+
+import json
 
 from cmp.models import Country
 
-csv_file = "/tmp/countries.csv"
-
-with open(csv_file, "r") as f:
-    for line in f:
-        line = line.strip()
-        if line.startswith("#"):
-            continue
-        id, name, alpha2, alpha3, old_id, blah = line.split(",")
-        c = Country(name=name, alpha2=alpha2, alpha3=alpha3, old_id=old_id)
-        print(c)
-        c.save()
+with open('countries.json') as f:
+    data = json.load(f) 
+    for country in data:
+        print(country['name']['common'])
