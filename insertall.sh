79eba2778e6bd922d34bddd78bc51536c4a349f2
David Morris <gm3dmo@github.com>
2023-11-08 13:58:38 +0000

    change models add inserter

---
+++ insertall.sh
@@ -0,0 +1,7 @@
+python manage.py runscript insert-all-countries
+python manage.py runscript insert-all-cemeteries
+python manage.py runscript insert-all-pow-camps
+python manage.py runscript insert-all-ranks
+python manage.py runscript insert-all-decorations
+python manage.py runscript insert-all-companies
+python manage.py runscript insert-all-soldiers
